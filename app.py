2c010483d4f3f057392c964188ea559bc5307cb6
riya063 <mrawat8186@gmail.com>
2025-06-17 14:59:37 +0530

    Initial commit with Dockerized Flask app

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import flask
- 
-app= Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hi from a Dockerized flask application"
+def hello():
+    return "🚀 Hello from Dockerized Flask App!"
 
-if __name__=='__main__':
-    app.run(host:0.0.0.0,port:5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
